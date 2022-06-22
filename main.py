7f5b3dd5dee0e0673ad931e4d5ebbc3240e92b3e
robertay <94378311+robyuk@users.noreply.github.com>
2022-06-22 11:08:12 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,15 @@
+from pathlib import Path
+
+root_dir=Path('files')
+file_paths=root_dir.glob("**/*")
+
+for path in file_paths:
+  if path.is_file():
+    print(path)
+    parent_folder=path.parts[-2]
+    print(parent_folder)
+    new_filename=parent_folder + '-' + path.name
+    print(new_filename)
+    new_filepath=path.with_name(new_filename)
+    path.rename(new_filepath)
+  
